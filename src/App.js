3d7f240f607169f73c022b9901d999ecd7658483
TonyS <tonysilvasd@gmail.com>
2019-08-08 16:08:53 -0700

    now using React Router and withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+const HatsPage = (props) => (
+  <h1>Hey there mr bill!!!</h1>
+  );
+
+function App(props) {
+
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/hats" component={HatsPage}/> 
+      </Switch>
     </div>
   );
 }
